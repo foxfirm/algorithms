e3d44da04333dfeb54b9e67ba8cf98997c787515
foxfirm <349907447@qq.com>
2019-08-07 00:01:06 +0800

    add algorithms maven project

---
+++ introduction_to_algorithms/src/main/java/com/foxfirm/algorithms/sortbase/InsertionSort.java
@@ -0,0 +1,15 @@
+package com.foxfirm.algorithms.sortbase;
+
+/**
+ * InsertionSort
+ *
+ * @author huligang
+ * @version 1.0
+ * @since 2019-08-06
+ */
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        System.out.println("Insertion-sort!");
+    }
+}
